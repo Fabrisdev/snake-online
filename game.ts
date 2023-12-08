935d80121a229a2f8cf1697812ed5c05e5bdca25
fabrisdev <ezepuntocom@hotmail.com>
2023-12-07 22:22:23 -0300

    add and remove player methods

--- game.ts
+++ game.ts
@@ -2,13 +2,18 @@ import { type Player } from './player'
 
 export class Game {
   private readonly host: Player
-  private readonly players: Player[]
+  private readonly players: Map<string, Player>
 
   constructor (host: Player) {
     this.host = host
+    this.players = new Map()
   }
 
-  addPlayer (player: Player) {
+  addPlayer (id: string, player: Player) {
+    this.players.set(id, player)
+  }
 
+  removePlayer (id: string) {
+    this.players.delete(id)
   }
 }
