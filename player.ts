33d812a9d897ffad6f1c2ed04a5e151eed10070a
fabrisdev <ezepuntocom@hotmail.com>
2023-12-08 00:49:01 -0300

    new player class

--- player.ts
+++ player.ts
@@ -1,5 +1,4 @@
 import { getRandomColor } from './utils'
-import { players } from './game-manager'
 
 export type Direction = 'up' | 'down' | 'left' | 'right'
 
@@ -13,18 +12,12 @@ export interface Position {
   y: number
 }
 
-export interface Player {
+export interface PlayerA {
   name: string
   position: Position
   direction: Direction
   color: string
   body: Position[]
-}
-
-interface RegisterPlayer {
-  clientId: string
-  players: typeof players
-  playerData: PlayerData
 }
 
 export function registerPlayer ({ clientId, players, playerData }: RegisterPlayer): void {
@@ -99,5 +92,73 @@ function moveHead (player: Player) {
 function moveBody (player: Player) {
   for (let i = player.body.length - 1; i >= 0; i--) {
     player.body[i] = structuredClone(player.body[i - 1]) ?? structuredClone(player.position)
+  }
+}
+
+export default class Player {
+  readonly name
+  readonly color
+  private readonly direction
+  private readonly head: Position
+  private readonly body: Position[] = []
+
+  constructor (name: string, direction: Direction, color: string) {
+    this.name = name
+    this.color = color
+    this.direction = direction
+    this.head = { x: 0, y: 0 }
+  }
+
+  move () {
+    this.moveBody()
+    this.moveHead()
+  }
+
+  private moveHead () {
+    if (this.direction === 'up') this.head.y -= 1
+    if (this.direction === 'down') this.head.y += 1
+    if (this.direction === 'left') this.head.x -= 1
+    if (this.direction === 'right') this.head.x += 1
+  }
+
+  private moveBody () {
+    for (let i = this.body.length - 1; i >= 0; i--) {
+      this.body[i] = structuredClone(this.body[i - 1]) ?? structuredClone(this.head)
+    }
+  }
+
+  hasCollisioned (players: Player[], mapSize: number) {
+    return this.collisionedAgainstPlayers(players) || this.collisionedAgainstWalls([this.head, ...this.body], mapSize)
+  }
+
+  private collisionedAgainstWalls (parts: Position[], mapSize: number) {
+    return parts.some(({ x, y }) => {
+      return x < 0 || x >= mapSize ||
+             y < 0 || y >= mapSize
+    })
+  }
+
+  private collisionedAgainstPlayers (players: Player[]) {
+    return [...players.values()].some(somePlayer => {
+      if (somePlayer === this) {
+        return this.collisionedAgainstItself()
+      }
+      return this.collisionedAgainstOtherPlayers(somePlayer)
+    })
+  }
+
+  private collisionedAgainstItself () {
+    for (let i = 0; i < this.body.length; i++) {
+      if (this.head.x === this.body[i].x && this.head.y === this.body[i].y) return true
+    }
+    return false
+  }
+
+  private collisionedAgainstOtherPlayers (somePlayer: Player) {
+    if (somePlayer.head.x === this.head.x && somePlayer.head.y === this.head.y) return true
+    for (let i = 0; i < somePlayer.body.length; i++) {
+      if (this.head.x === somePlayer.body[i].x && this.head.y === somePlayer.body[i].y) return true
+    }
+    return false
   }
 }
