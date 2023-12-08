67afeb5d2e84e2527f2a05441ae45d354f7b210e
fabrisdev <ezepuntocom@hotmail.com>
2023-12-07 22:26:36 -0300

    map size is customizable now

--- game.ts
+++ game.ts
@@ -1,12 +1,16 @@
 import { type Player } from './player'
 
 export class Game {
+  private readonly mapSize: number
+  private readonly playerSize: number
   private readonly host: Player
   private readonly players: Map<string, Player>
 
-  constructor (host: Player) {
+  constructor (host: Player, mapSize?: number, playerSize?: number) {
     this.host = host
     this.players = new Map()
+    this.mapSize = mapSize ?? 500
+    this.playerSize = playerSize ?? 25
   }
 
   addPlayer (id: string, player: Player) {
