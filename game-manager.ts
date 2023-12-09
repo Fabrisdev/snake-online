c132e8fc044a6daa6e0d88ce0d537bda86385f8b
fabrisdev <ezepuntocom@hotmail.com>
2023-12-09 00:22:00 -0300

    updated handleMessage function

--- game-manager.ts
+++ game-manager.ts
@@ -1,11 +1,19 @@
-export function handleMessage (clientId: string, data: PlayerData) {
-  if (!players.has(clientId)) {
-    registerPlayer({
-      clientId,
-      players,
-      playerData: data
-    })
-    return
-  }
-  updatePlayerDirection(clientId, data.direction)
+interface JoinMessage {
+  type: 'join'
+  name: string
+}
+
+interface LeaveMessage {
+  type: 'leave'
+}
+
+interface ChangeDirectionMessage {
+  type: 'changeDirection'
+  direction: 'left' | 'right' | 'down' | 'up'
+}
+
+type ClientMessage = JoinMessage | LeaveMessage | ChangeDirectionMessage
+
+export function handleMessage (clientId: string, data: ClientMessage) {
+
 }
