f39a69fa8e4ab2303c594e91b89a6eeabf2e9e9b
fabrisdev <ezepuntocom@hotmail.com>
2023-12-09 01:08:02 -0300

    new game manager class that handles logic behind

--- game-manager.ts
+++ game-manager.ts
@@ -23,7 +23,41 @@ interface ChangeDirectionMessage {
 
 type ClientMessage = CreateGameMessage | JoinMessage | LeaveMessage | ChangeDirectionMessage
 
-const games = new Map<string, Game>()
+class GameManager extends Map<string, Game> {
+  create (clientId: string) {
+    games.set(clientId, new Game())
+  }
+
+  changePlayerDirection (clientId: string, direction: Direction) {
+    this.forEach(game => {
+      game.changePlayerDirection(clientId, direction)
+    })
+  }
+
+  leaveAll (clientId: string) {
+    this.forEach(game => {
+      game.removePlayer(clientId)
+    })
+  }
+
+  join (clientId: string, message: JoinMessage) {
+    const game = this.get(message.gameId)
+    if (game === undefined) {
+      sendMessage(clientId, 'The specified game does not exist.')
+      return
+    }
+    const player = new Player(message.name)
+    game.addPlayer(clientId, player)
+  }
+
+  changeDirection (clientId: string, direction: Direction) {
+    this.forEach(game => {
+      game.changePlayerDirection(clientId, direction)
+    })
+  }
+}
+
+const games = new GameManager()
 
 export function handleMessage (clientId: string, message: ClientMessage) {
   if (message.type === 'create') createGame(clientId)
